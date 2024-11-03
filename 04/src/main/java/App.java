5430bbaa1f23be63c06f164e9b549072409622be
Asifiwe BUHENDWA <mickaelbhd@gmail.com>
2024-11-03 10:38:37 +0200

    Creating 04

---
+++ 04/src/main/java/App.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+/**
+ *
+ * @author mickael-angebhd
+ */
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
